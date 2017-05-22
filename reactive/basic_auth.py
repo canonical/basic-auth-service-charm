f6dc8a3fab19216ac10a4b7d3d285f883de9c93c
Alberto Donato <alberto.donato@gmail.com>
2017-05-22 12:27:35 +0200

    Rework states and config.

--- reactive/basic_auth.py
+++ reactive/basic_auth.py
@@ -2,10 +2,16 @@ import glob
 import os
 import os.path
 import subprocess
-import yaml
 
-from charms import reactive
-import charms.layer.snap
+from charms.reactive import (
+    when,
+    when_not,
+    when_any,
+    when_file_changed,
+    set_state,
+    remove_state
+)
+from charms.layer import snap
 
 from charmhelpers.core import (
     hookenv,
@@ -14,13 +20,17 @@ from charmhelpers.core import (
 )
 from charmhelpers.contrib.charmsupport import nrpe
 
-SNAP_FILE_NAME = "basic-auth-service_*.snap"
-SNAP_CONFIG_PATH = "/var/snap/basic-auth-service/current/config.yaml"
-SNAP_SERVICES = ("snap.basic-auth-service.basic-auth-service",)
-ALEMBIC_TEMPLATE = "templates/alembic.ini"
-ALEMBIC_CONFIG_PATH = "/var/snap/basic-auth-service/current/alembic.ini"
+SNAP_FILE_NAME = 'basic-auth-service_*.snap'
+SNAP_SERVICES = ('snap.basic-auth-service.basic-auth-service',)
+TEMPLATES_DIR = (
+    '/snap/basic-auth-service/current/usr/share/basic-auth-service/templates')
+ALEMBIC_TEMPLATE = 'alembic.ini'
+ALEMBIC_CONFIG_PATH = '/var/snap/basic-auth-service/current/alembic.ini'
+SNAP_CONFIG_TEMPLATE = 'config.yaml'
+SNAP_CONFIG_PATH = '/var/snap/basic-auth-service/current/config.yaml'
 
-PORT = 8080
+
+APPLICATION_PORT = 8080
 
 
 def charm_state(state):
@@ -28,59 +38,54 @@ def charm_state(state):
     return 'basic-auth-service.{}'.format(state)
 
 
-@reactive.hook("install")
-def install_snap():
-    hookenv.status_set("maintenance", "Installing basic-auth-service snap.")
-    charm_dir = os.environ["JUJU_CHARM_DIR"]
+@when_not(charm_state('installed'))
+def install():
+    hookenv.status_set('maintenance', 'Installing basic-auth-service snap.')
+    charm_dir = os.environ['JUJU_CHARM_DIR']
     snap_path = os.path.join(charm_dir, SNAP_FILE_NAME)
     snap_file = glob.glob(snap_path)[0]
-    charms.layer.snap._install_local(snap_file)
-    reactive.set_state(charm_state("installed"))
-    reactive.set_state(charm_state("db-update"))
-    reactive.remove_state(charm_state("configured"))
+    snap._install_local(snap_file)
+    set_state(charm_state('installed'))
+    # db migration should be applied once the service is configured
+    set_state(charm_state('db-update'))
 
 
-@reactive.when("website.available")
+@when(charm_state('installed'), 'website.available')
 def configure_website(website):
     """Configure reverse proxy to point to our application."""
-    website.configure(port=PORT)
+    website.configure(port=APPLICATION_PORT)
 
 
-@reactive.when("database.master.available", charm_state("installed"))
+@when(charm_state('installed'), 'database.master.available')
 def configure_basic_auth_service(pgsql):
     """Configure the service when the database is available."""
-    hookenv.status_set("maintenance", "Writing config.")
-    snap_config = {
-        "database": {
-            "dsn": pgsql.master.uri,
-        }}
-    with open(SNAP_CONFIG_PATH, "w") as config_file:
-        yaml.dump(snap_config, stream=config_file)
-    hookenv.status_set("maintenance", "Service configuration updated.")
-
-    charm_dir = os.environ["JUJU_CHARM_DIR"]
-    context = {"sqlalchemy_postgres_uri": pgsql.master.uri}
+    hookenv.status_set('maintenance', 'Writing config.')
+    # write service config
+    context = {'db_dsn': pgsql.master.uri, 'app_port': APPLICATION_PORT}
+    templating.render(
+        SNAP_CONFIG_TEMPLATE, SNAP_CONFIG_PATH, context, perms=0o440,
+        templates_dir=TEMPLATES_DIR)
+    hookenv.status_set('maintenance', 'Service configuration updated.')
+    # write alembic config
+    context = {'sqlalchemy_url': pgsql.master.uri}
     templating.render(
         ALEMBIC_TEMPLATE, ALEMBIC_CONFIG_PATH, context, perms=0o440,
-        templates_dir=charm_dir)
-    reactive.set_state(charm_state("configured"))
-    hookenv.status_set("active", "Configuration done.")
+        templates_dir=TEMPLATES_DIR)
+    hookenv.status_set('active', 'Configuration done.')
+    set_state(charm_state('configured'))
 
 
-@reactive.when('nrpe-external-master.available')
-@reactive.when_not('nrpe-external-master.initial-config')
+@when('nrpe-external-master.available')
+@when_not(charm_state('nrpe-initial-config'))
 def initial_nrpe_config(nagios=None):
-    reactive.set_state('nrpe-external-master.initial-config')
     update_nrpe_config(nagios)
+    set_state(charm_state('nrpe-initial-config'))
 
 
-@reactive.when(charm_state("installed"),
-               charm_state("configured"),
-               "nrpe-external-master.available")
-@reactive.when_any('config.changed.nagios_context',
-                   'config.changed.nagios_servicegroups')
+@when(charm_state('configured'), 'nrpe-external-master.available')
+@when_any('config.changed.nagios_context',
+          'config.changed.nagios_servicegroups')
 def update_nrpe_config(unused=None):
-
     hostname = nrpe.get_nagios_hostname()
     current_unit = nrpe.get_nagios_unit_name()
     nrpe_setup = nrpe.NRPE(hostname=hostname, primary=True)
@@ -90,10 +95,9 @@ def update_nrpe_config(unused=None):
     nrpe_setup.write()
 
 
-@reactive.when_not('nrpe-external-master.available')
-@reactive.when('nrpe-external-master.initial-config')
+@when_not('nrpe-external-master.available')
+@when(charm_state('nrpe-initial-config'))
 def remove_nrpe_config(nagios=None):
-    reactive.remove_state('nrpe-external-master.initial-config')
 
     hostname = nrpe.get_nagios_hostname()
     nrpe_setup = nrpe.NRPE(hostname=hostname)
@@ -101,25 +105,25 @@ def remove_nrpe_config(nagios=None):
     for service in SNAP_SERVICES:
         nrpe_setup.remove_check(shortname=service)
 
+    remove_state(charm_state('nrpe-initial-config'))
 
-@reactive.when("database.master.available",
-               charm_state("db-update"),
-               charm_state("configured"))
+
+@when(charm_state('configured'), charm_state('db-update'),
+      'database.master.available')
 def perform_database_migrations(pgsql):
-    hookenv.status_set("maintenance", "Performing database migrations.")
+    hookenv.status_set('maintenance', 'Performing database migrations.')
     subprocess.run(
-        ["/snap/bin/basic-auth-service.alembic", "-c", ALEMBIC_CONFIG_PATH,
-         "upgrade", "head"],
+        ['/snap/bin/basic-auth-service.alembic', '-c', ALEMBIC_CONFIG_PATH,
+         'upgrade', 'head'],
         stderr=subprocess.PIPE, check=True)
     # Clear the state marking for needing DB migrations.
-    reactive.remove_state(charm_state("db-update"))
-    hookenv.status_set("maintenance", "Database migrations applied.")
+    hookenv.status_set('maintenance', 'Database migrations applied.')
+    remove_state(charm_state('db-update'))
 
 
-@reactive.when_file_changed(SNAP_CONFIG_PATH)
+@when_file_changed(SNAP_CONFIG_PATH)
 def restart_service():
-    """Perform a service restart."""
     hookenv.status_set(
-        "maintenance", "Restarting service: basic-auth-service.")
-    host.service_restart("snap.basic-auth-service.basic-auth-service")
-    hookenv.status_set("active", "Service up and running.")
+        'maintenance', 'Restarting service: basic-auth-service.')
+    host.service_restart('snap.basic-auth-service.basic-auth-service')
+    hookenv.status_set('active', 'Service up and running.')
